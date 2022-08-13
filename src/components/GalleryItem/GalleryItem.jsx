6ef28aaa8a395c3b02a12065bc8f2761057502f6
Madeline Prickette <madelineprickette@gmail.com>
2022-08-13 18:17:01 -0500

    went back to main

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -3,14 +3,14 @@ import {useState} from 'react';
 
 function GalleryItem({pic, likeCounter}) {
     // Create this variable to set a status of a clicked image or description
-    let [imageClicked, setImageClicked] = useState(false)
+    const [imageClicked, setImageClicked] = useState(false)
 
     console.log(imageClicked); // Should show true or false in the console
     return(
         <>
             <div onClick={() => setImageClicked(!imageClicked)}>
                 {/* Ternary operators to switch between image and description, ? asks "if true" and : represents "else" */}
-                {imageClicked === true ? <><p className="pTagSize">{pic.description}</p></> : <><img src={pic.path} ></img></>}
+                {imageClicked === true ? <p className="pTagSize">{pic.description}</p> : <img src={pic.path} ></img>}
             </div>
             <><button onClick={() => likeCounter(pic)}>❤️</button><p>Likes: {pic.likes}</p></>
         </>
