80a20a795521bf371c0e25d2c8d398d4a8730ed5
Madeline Prickette <madelineprickette@gmail.com>
2022-08-14 12:58:38 -0500

    got put to work omg

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -5,25 +5,42 @@ const pool = require('../modules/pool');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
-// PUT Route
+// PUT Route after transferring data to db ----------
 router.put('/like/:id', (req, res) => {
     console.log(req.params);
     const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    console.log(galleryId); // This is the component/object id number
+
+    let queryText = 
+    `
+    UPDATE  "gallery"
+    SET "likes" = "likes" + 1
+    WHERE "id" = $1;
+    `
+
+    pool.query(queryText, [galleryId])
+        .then( (results) => {
+            res.sendStatus(200)
+        }).catch( (err) => {
+            console.error('ERROR IN SERVER PUT', err);
+            res.sendStatus(500);
+        })
+
+    // for(const galleryItem of galleryItems) {
+    //     if(galleryItem.id == galleryId) {
+    //         galleryItem.likes += 1;
+    //     }
+    // }
+    // res.sendStatus(200);
 }); // END PUT Route
 
-// GET Route
+// GET Route after transferring data to db ----------
 router.get('/', (req, res) => {
     console.log('in /GET');
     
     let queryText = 
     `
-    SELECT * FROM "gallery";
+    SELECT * FROM "gallery" ORDER BY "id";
     `;
 
     pool.query(queryText)
