924fd7b42a81b93a68d385639beb871274be1fe1
Madeline Prickette <madelineprickette@gmail.com>
2022-08-14 16:35:55 -0500

    did more styling, buttons, text, centering.... ugh

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -11,11 +11,12 @@ function GalleryItem({pic, likeCounter, deletePic}) {
     return(
         <div className='spacing-posts'>
             <div className='post-border'>
+                <p className='postition-name'>urstrayedaway</p>
                 <div onClick={() => setImageClicked(!imageClicked)} className='top-margin'>
                     {/* Ternary operators to switch between image and description, ? asks "if true" and : represents "else" */}
                     {imageClicked === true ? <p className="pTagSize">{pic.description}</p> : <img src={pic.path}></img>}
                 </div>
-                <div className='bottom-margin'><div className='inline padding-right-270'><button onClick={() => likeCounter(pic)} className="inline margin-right-7">❤️</button><p className='inline'>Likes: {pic.likes}</p></div><button onClick={() => deletePic(pic)} className='inline'>Delete</button></div>
+                <div className='bottom-margin'><div className='inline padding-right-270 traditional-text'><button onClick={() => likeCounter(pic)} className="inline margin-right-7 button-radius">❤️</button><p className='inline'>Likes: {pic.likes}</p></div><button onClick={() => deletePic(pic)} className='inline traditional-text button-radius'>Delete</button></div>
             </div>
         </div>
     )
