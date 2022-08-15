1233b6e1e250bd6d19f86e9d57d86a17d6e811f1
Madeline Prickette <madelineprickette@gmail.com>
2022-08-15 00:52:34 -0500

    fixed input border radius

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -35,7 +35,7 @@ function App() {
         <header className="App-header">
           <div className="align-items">
             <h1 className="App-title gradient-text marshmellow-text inline-block">Catstagram</h1>
-            <input placeholder="search" className="input-styling"/><button className='search-button'>🔍</button>
+            <input placeholder="search" className="input-styling" value="search"/><button className='search-button'>🔍</button>
             <p className="margin-left p-tag">urstrayedaway</p>
             <img className="headerImage inline-block" src="images/profile-img.jpg"/>
           </div>
